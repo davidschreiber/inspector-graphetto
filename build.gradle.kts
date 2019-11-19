6b98c31fc36421be23f8a0ae7a10b07d53f6f891
David Schreiber-Ranner <david@pspdfkit.com>
2019-11-19 20:59:16 +0100

    Add preliminary test configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.60"
+}
+
 group = "at.droiddave.grapher"
 version = "1.0-SNAPSHOT"
 
+repositories {
+    mavenCentral()
+}
+
+val compileKotlin: KotlinCompile by tasks
+val compileTestKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions { jvmTarget = "1.8" }
+compileTestKotlin.kotlinOptions { jvmTarget = "1.8" }
+
+dependencies {
+    testImplementation("io.mockk:mockk:1.9.3")
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.3.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.2")
+
+
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/test/kotlin/Test.kt
@@ -0,0 +1,16 @@
+import io.kotlintest.fail
+import io.kotlintest.specs.StringSpec
+
+class Test : StringSpec({
+
+    "This is some test" {
+    }
+
+    "This is some other test" {
+
+    }
+
+    "This is some other testtestqq" {
+
+    }
+})
