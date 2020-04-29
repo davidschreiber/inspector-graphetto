531699aa57c6894573947c494f00e0fddecdf98c
David Schreiber-Ranner <david@pspdfkit.com>
2020-04-29 22:19:19 +0200

    Add AGP 3.6.3 functional tests

--- src/test/kotlin/at/droiddave/graphetto/tests/AndroidIntegrationTest.kt
+++ src/test/kotlin/at/droiddave/graphetto/tests/AndroidIntegrationTest.kt
@@ -1,46 +1,52 @@
 package at.droiddave.graphetto.tests
 
 import at.droiddave.graphetto.tests.utils.*
+import io.kotlintest.specs.FunSpec
 import io.kotlintest.specs.StringSpec
 
-class AndroidIntegrationTest : StringSpec() {
+class AndroidIntegrationTest : FunSpec() {
     private val directoryRule = TestDirectoryListener()
     override fun listeners() = listOf(directoryRule)
 
     init {
-        "Test plugin instantiation using plugin ID" {
-            val projectDir = directoryRule.initializeWithResourceDirectory("/fixtures/android-agp-3.5.2")
-            gradleRunner(projectDir)
-                .build()
-        }
+        val androidGradlePluginVersions = listOf("3.5.2", "3.6.3")
+        androidGradlePluginVersions.forEach { androidGradlePluginVersion ->
+            context("With Android Gradle Plugin $androidGradlePluginVersion") {
+                test("Test plugin instantiation using plugin ID") {
+                    val projectDir = directoryRule.initializeWithResourceDirectory("/fixtures/android-agp-$androidGradlePluginVersion")
+                    gradleRunner(projectDir)
+                            .build()
+                }
 
-        "Report task graph for :assembleDebug" {
-            val projectDir = directoryRule.initializeWithResourceDirectory("/fixtures/android-agp-3.5.2")
-            gradleRunner(projectDir)
-                .withArguments("assembleDebug")
-                .build()
+                test("Report task graph for :assembleDebug") {
+                    val projectDir = directoryRule.initializeWithResourceDirectory("/fixtures/android-agp-$androidGradlePluginVersion")
+                    gradleRunner(projectDir)
+                            .withArguments("assembleDebug")
+                            .build()
 
-            val reportFile = projectDir.resolve("build/reports/taskGraph/graph.dot")
-            val actualGraph = loadGraphFromFile(reportFile)
-            val expectedGraph = loadGraphFromTestResources("/results/android-agp-3.5.2.dot")
-            actualGraph shouldBeIsomorphTo expectedGraph
-        }
+                    val reportFile = projectDir.resolve("build/reports/taskGraph/graph.dot")
+                    val actualGraph = loadGraphFromFile(reportFile)
+                    val expectedGraph = loadGraphFromTestResources("/results/android-agp-$androidGradlePluginVersion.dot")
+                    actualGraph shouldBeIsomorphTo expectedGraph
+                }
 
-        "Works if report file already exists" {
-            val projectDir = directoryRule.initializeWithResourceDirectory("/fixtures/android-agp-3.5.2")
-            projectDir.resolve("build/reports/taskGraph/graph.dot").apply {
-                parentFile.mkdirs()
-                writeText("some content")
+                test("Works if report file already exists") {
+                    val projectDir = directoryRule.initializeWithResourceDirectory("/fixtures/android-agp-$androidGradlePluginVersion")
+                    projectDir.resolve("build/reports/taskGraph/graph.dot").apply {
+                        parentFile.mkdirs()
+                        writeText("some content")
+                    }
+
+                    gradleRunner(projectDir)
+                            .withArguments("assembleDebug")
+                            .build()
+
+                    val reportFile = projectDir.resolve("build/reports/taskGraph/graph.dot")
+                    val actualGraph = loadGraphFromFile(reportFile)
+                    val expectedGraph = loadGraphFromTestResources("/results/android-agp-$androidGradlePluginVersion.dot")
+                    actualGraph shouldBeIsomorphTo expectedGraph
+                }
             }
-
-            gradleRunner(projectDir)
-                .withArguments("assembleDebug")
-                .build()
-
-            val reportFile = projectDir.resolve("build/reports/taskGraph/graph.dot")
-            val actualGraph = loadGraphFromFile(reportFile)
-            val expectedGraph = loadGraphFromTestResources("/results/android-agp-3.5.2.dot")
-            actualGraph shouldBeIsomorphTo expectedGraph
         }
     }
 }
